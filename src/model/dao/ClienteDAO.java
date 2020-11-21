0f496334b39e63b903176be3fbb8a6a45aac4ab7
UsuarioPC <UsuarioPC@DESKTOP-UI82SCL>
2020-11-20 23:10:01 -0300

    .

--- src/model/dao/ClienteDAO.java
+++ src/model/dao/ClienteDAO.java
@@ -9,18 +9,18 @@ import connection.ConnectionFactory;
 import model.bean.Cliente;
 
 	public class ClienteDAO {
-		public void create (Cliente c);
+		public void create (Cliente c) {
 		Connection con = ConnectionFactory.getConnection();
 		PreparedStatement stmt = null;
 		
 		try {
-			stmt = con.prepareStatement("INSERT INTO CLIENTE(nome, cpf, endereco, telefone, idade) "+"(?,?,?,?,?,?)");
+			stmt = con.prepareStatement("INSERT INTO CLIENTE(nome, cpf, endereco, telefone, idade) "+"(?,?,?,?,?)");
 		
 			stmt.setString(1, c.getNome());
 			stmt.setString(2, c.getCpf());
 			stmt.setString(3, c.getEndereco());
 			stmt.setString(4, c.getTelefone());
-			stmt.setInt(5, c.isIdade());
+			stmt.setInt(5, c.getIdade());
 		
 			stmt.executeUpdate();
 			JOptionPane.showMessageDialog(null, "Cliente Salvo! ");
