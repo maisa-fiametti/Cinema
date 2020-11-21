954e539c6d1344276bae97dfe63f2729209815f0
maisa-fiametti <53800850+maisa-fiametti@users.noreply.github.com>
2020-11-20 23:38:53 -0300

    Update cliente.sql

--- cliente.sql
+++ cliente.sql
@@ -1,1 +1,21 @@
-CREATE TABLE cliente ( idCliente int NOT NULL AUTO_INCREMENT PRIMARY KEY, nome varchar(50), cpf varchar(11), endereco varchar(70), telefone varchar(15), idade int );
+CREATE DATABASE cinema; 
+USE cinema;
+
+CREATE TABLE filme ( 
+idFilme int NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+titulo varchar(50), 
+tempo int, 
+imagem3d tinyint, 
+dublado tinyint, 
+sinopse varchar(500), 
+categoria varchar(20) 
+);
+
+CREATE TABLE cliente ( 
+idCliente int NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+nome varchar(50), 
+cpf varchar(11), 
+endereco varchar(70), 
+telefone varchar(15), 
+idade int 
+);
