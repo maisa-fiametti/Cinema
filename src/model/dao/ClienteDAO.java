7873a890c84cf8d6bb540d6f4cbf94aa1f2ddd48
UsuarioPC <UsuarioPC@DESKTOP-UI82SCL>
2020-12-02 17:11:12 -0300

    atualização classe Cliente.DAO

--- src/model/dao/ClienteDAO.java
+++ src/model/dao/ClienteDAO.java
@@ -1,12 +1,16 @@
 package model.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 
 import connection.ConnectionFactory;
 import model.bean.Cliente;
+import model.bean.Filme;
 
 	public class ClienteDAO {
 		public void create (Cliente c) {
@@ -33,4 +37,36 @@ import model.bean.Cliente;
 		}
 
 	}
+		public List<Cliente> read(){
+			Connection con = ConnectionFactory.getConnection();
+			PreparedStatement stmt = null;
+			ResultSet rs = null;
+			
+			List<Cliente> clientes = new ArrayList<>();
+
+			try {
+				stmt = con.prepareStatement("SELECT * FROM cliente;");
+				rs = stmt.executeQuery();
+				
+				while (rs.next()) {
+					Cliente c = new Cliente();
+					c.setIdCliente(rs.getInt("idCliente"));
+					c.setNome(rs.getString("nome"));
+					c.setCpf(rs.getString("cpf"));
+					c.setEndereco(rs.getString("endereco"));
+					c.setTelefone(rs.getString("telefone"));
+					c.setIdade(rs.getInt("idade"));
+					clientes.add(c);
+				}
+				
+			} catch (SQLException e) {
+				JOptionPane.showMessageDialog(null, "Erro ao buscar as informações do BD"+ e);
+				e.printStackTrace();
+			}finally {
+				ConnectionFactory.closeConnection(con, stmt, rs);
+			}
+			
+			return clientes;
+		}
+			
 }
